2b4cdec5298df9dda2e24c15f705226f4ff4c02e
RamyaJaishankar <84244037+RamyaJaishankar@users.noreply.github.com>
2021-07-07 23:36:28 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Age {
+	public static void main(String[] args) {
+		int age=29 ;
+		int age2=39;
+		int res=age+age2;
+		if(age>30 && age2<40)
+		{
+			System.out.println("he is adult"); 
+			System.out.println("two age is" + res+ "family");
+			
+		}
+		else 
+		
+			System.out.println("uncle");
+		}
+}
+
